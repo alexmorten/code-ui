43103d95f4a84c83536a16909638c2b40cb18dea
alexmorten <alexandrius.martinus@gmx.de>
2017-09-11 09:35:56 +0200

    right icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './css/App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {NavLink,withRouter} from 'react-router-dom';
